636306d26326f51f51e678622b6bfa90675286e1
Amit Kumar <32258879+geekospot@users.noreply.github.com>
2017-12-29 00:33:42 +0530

    Initial files uploaded

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div id="outer">
+    <h1 id="heading">Validation Form</h1>
+    <!--INPUTS-->
+    <form method="post" action="index.php">
+    
+        <input type="text" id="name" placeholder="NAME"><br><br>
+        <input type="text" id="email" placeholder="E-MAIL"><br><br>
+        <input type="text" id="num" placeholder="NUMBER"><br><br>
+        <input type="submit" id="submit" value="SUBMIT"><br><br>
+    </form>
+    
+</div>
+</body>
+</html>
+
+<?php
+    $
+?>
