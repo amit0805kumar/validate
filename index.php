d54dcbe94514ea31830bd670a730c02853f1b6d8
Amit Kumar <32258879+geekospot@users.noreply.github.com>
2017-12-30 00:53:11 +0530

    max length validated

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 <head>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="stle.css">
+     <script src="script.js"></script>
 </head>
 <body>
 <div id="outer">
@@ -8,9 +9,13 @@
     <!--INPUTS-->
     <form method="post" action="index.php">
     
-        <input type="text" id="loginid" placeholder="LOGIN"><br><br>
-        <input type="password" id="password" placeholder="PASSWORD"><br><br>
-        <input type="password" id="cpassword" placeholder="CONFIRM PASSWORD"><br><br>
+        <input type="text" id="loginid" placeholder="LOGIN" onkeyup="UpdateLength('loginid','sizelogin')"  maxlength="25" ><br/>
+        <div id="sizelogin"></div>
+        <input type="password" id="password" placeholder="PASSWORD"  onkeyup="UpdateLength('password','sizepass')" maxlength="20"><br>
+        <div id="sizepass"></div>
+        
+        <input type="password" id="cpassword" placeholder="CONFIRM PASSWORD"  onkeyup="UpdateLength('cpassword','sizecpass')" maxlength="20"><br>
+        <div id="sizecpass"></div>
         <select class="gender" placeholder="GENDER">
             <option class="gender" style="color:blue;">-SELECT-</option>
             <option class="gender" value="MALE">MALE</option>
@@ -18,7 +23,8 @@
             <option class="gender" value="TRANs">TRANS</option>
             
         </select><br><br>
-        <input type="text" id="email" placeholder="E-MAIL"><br><br>
+        <input type="text" id="email" placeholder="E-MAIL"  onkeyup="UpdateLength('email','sizeemail')" maxlength="25"><br>
+        <div id="sizeemail"></div>
         
         <input type="submit" id="submit" value="SUBMIT"><br><br>
     </form>
