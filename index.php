7aad619d3c21992c2e820ff054ad995c6a5e46b4
Amit Kumar <32258879+geekospot@users.noreply.github.com>
2017-12-30 16:52:37 +0530

    new fonts applied

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <html>
 <head>
-    <link rel="stylesheet" href="stle.css">
+    <link rel="stylesheet" href="style.css">
      <script src="script.js"></script>
+   <link href="https://fonts.googleapis.com/css?family=Concert+One|Fredoka+One|Permanent+Marker|Righteous" rel="stylesheet">
 </head>
 <body>
 <div id="outer">
@@ -9,21 +10,21 @@
     <!--INPUTS-->
     <form method="post" action="index.php">
     
-        <input type="text" id="loginid" placeholder="LOGIN" onkeyup="UpdateLength('loginid','sizelogin')"  maxlength="25" ><br/>
+        <input type="text" id="loginid" placeholder="LOGIN" onkeyup="UpdateLength('loginid','sizelogin')"  maxlength="25" name="login" ><br/>
         <div id="sizelogin"></div>
-        <input type="password" id="password" placeholder="PASSWORD"  onkeyup="UpdateLength('password','sizepass')" maxlength="20"><br>
+        <input type="password" id="password" name="pass" placeholder="PASSWORD"  onkeyup="UpdateLength('password','sizepass')" maxlength="20"><br>
         <div id="sizepass"></div>
         
-        <input type="password" id="cpassword" placeholder="CONFIRM PASSWORD"  onkeyup="UpdateLength('cpassword','sizecpass')" maxlength="20"><br>
+        <input type="password" id="cpassword" name="cpass" placeholder="CONFIRM PASSWORD"  onkeyup="UpdateLength('cpassword','sizecpass')" maxlength="20"><br>
         <div id="sizecpass"></div>
-        <select class="gender" placeholder="GENDER">
+        <select class="gender" placeholder="GENDER" name="gender">
             <option class="gender" style="color:blue;">-SELECT-</option>
-            <option class="gender" value="MALE">MALE</option>
-            <option class="gender" value="FEMALE">FEMALE</option>
-            <option class="gender" value="TRANs">TRANS</option>
+            <option class="gender" value="M">MALE</option>
+            <option class="gender" value="F">FEMALE</option>
+            <option class="gender" value="T">TRANS</option>
             
         </select><br><br>
-        <input type="text" id="email" placeholder="E-MAIL"  onkeyup="UpdateLength('email','sizeemail')" maxlength="25"><br>
+        <input type="text" id="email" placeholder="E-MAIL" name="email"  onkeyup="UpdateLength('email','sizeemail')" maxlength="25"><br>
         <div id="sizeemail"></div>
         
         <input type="submit" id="submit" value="SUBMIT"><br><br>
