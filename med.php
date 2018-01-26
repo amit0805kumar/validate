388087bb617c7814568537f582c3f301f0b18ead
Amit Kumar <amit0805kumar@gmail.com>
2018-01-26 15:37:08 +0530

    scroll effect added

---
+++ med.php
@@ -0,0 +1,14 @@
+
+<?php
+    $database="validate";
+    $password="";
+    $servername="localhost";
+    $username="root";
+    $conn = new mysqli($servername,$username,$password,$database);
+?>
+<html>
+    <head>
+    </head><body>
+        <p>This is a paragraph</p>
+    </body>
+</html>
