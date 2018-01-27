9d9f8f06b42da054edb0b4fee5b152c65b2151dd
Amit Kumar <amit0805kumar@gmail.com>
2018-01-27 23:48:49 +0530

    Back-End Done

--- med.php
+++ med.php
@@ -43,6 +43,27 @@ echo
            
                 margin-left: 150px;
             }
+#cmd
+{
+width:130px;
+height:50px;
+font-family:sans;
+color:white;
+background:#d500f9;
+padding:5px;
+border:1px solid;
+border-radius:7px;
+margin-left:400px;
+font-size:20px;
+transition:all 0.3s;
+}
+#cmd:hover
+{
+cursor: pointer;
+color:#d500f9;
+background:white;
+margin-left:420px;
+}
         </style>
     </head><body>
     <div id="header">
@@ -53,13 +74,20 @@ $login=$_POST["login"];
 $pass=$_POST["pass"];
 $email=$_POST["email"];
 $gen=$_POST["gender"];
-            
+$sql="insert into inputs value ('$login','$pass','$gen','$email')";
+$conn->query($sql);
 echo    '<p id="text"><a style="background:#00DED1;">DATA</a> INSERTED</p>';
             
         }
+else
+{
+    echo    '<p id="text"><a style="color:red;">DATA NOT INSERTED</a></p>';
+}
  echo   '</div>';
 
-
+echo '<br>
+    <a href="index.html"><input type="button" id="cmd" value="GO Home &gt;"></input></a>
+';
 
   echo'</body>
 </html>';
