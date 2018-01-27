47497fc76ff1d9c308354c3a097252f0e1456841
Amit Kumar <amit0805kumar@gmail.com>
2018-01-27 18:13:46 +0530

    back end
    ~Php page created and designed for insertion confirmation

--- med.php
+++ med.php
@@ -5,10 +5,62 @@
     $servername="localhost";
     $username="root";
     $conn = new mysqli($servername,$username,$password,$database);
-?>
-<html>
+    if($conn->connect_error)
+    {
+    die("Connect failes: ".$conn->connect_error);
+    }
+echo
+    
+'<html>
     <head>
+        
+        <style>
+            *
+            {
+                margin: 0px;
+            }
+#header
+            {
+                margin-top: 200px;
+                width: 100%;
+                height: 200px;
+                background: purple;
+                position: relative;
+                
+            }
+#text
+            {
+                color: white;
+                 font-family: sans-serif;
+                font-size: 50px;
+                margin-left: 100px;
+                position: absolute;
+                margin-top: 50px;
+                transition: all 0.5s;
+            }
+#header:hover >#text
+            {
+           
+                margin-left: 150px;
+            }
+        </style>
     </head><body>
-        <p>This is a paragraph</p>
-    </body>
-</html>
+    <div id="header">
+        ';
+        if(!empty($_POST["login"]) && !empty($_POST["pass"]) && !empty($_POST["gender"]) && !empty($_POST["email"]))
+        {
+$login=$_POST["login"];
+$pass=$_POST["pass"];
+$email=$_POST["email"];
+$gen=$_POST["gender"];
+            
+echo    '<p id="text"><a style="background:#00DED1;">DATA</a> INSERTED</p>';
+            
+        }
+ echo   '</div>';
+
+
+
+  echo'</body>
+</html>';
+?>
